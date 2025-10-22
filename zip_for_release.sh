2ca8df8375d1bb6ca8f4fa3b004f55f3d6e4e77a
Sicong Huang <hugohuang1997@gmail.com>
2025-10-21 20:57:53 -0700

    add download_best_model.py to zip_for_release.sh

--- zip_for_release.sh
+++ zip_for_release.sh
@@ -15,6 +15,7 @@ cp model.py "$HW2_DIR/"
 cp train.py "$HW2_DIR/"
 cp util.py "$HW2_DIR/"
 cp requirements.txt "$HW2_DIR/"
+cp download_best_model.py "$HW2_DIR/"
 
 # Create the zip file from the temporary directory
 cd "$TEMP_DIR"
